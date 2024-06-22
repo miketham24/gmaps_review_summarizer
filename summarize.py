a1e8e674254653b1f6fe1e19224f23e82d455adc
miketham24 <michaelthamk@gmail.com>
2024-06-21 17:10:05 -0700

    added handle for case of 'more...' button

--- summarize.py
+++ summarize.py
@@ -16,7 +16,7 @@ url = "https://www.google.com/maps/place/Phnom+Penh+Restaurant/@49.2784175,-123.
 async def scrape_reviews(url):
     #headless = true as we don't want it to open a browser
     #await as we want line of code to wait until code is executed until going to next step
-    browser = await launch({"headless": False, "args":["--window-size=800,3200"]})
+    browser = await launch({"headless": True, "args":["--window-size=800,3200"]})
 
     page = await browser.newPage()
     await page.setViewport({"width": 800, "height": 3200})
@@ -26,6 +26,13 @@ async def scrape_reviews(url):
     # turns reviews div into a list
     elements = await page.querySelectorAll('.jftiEf')
     for element in elements:
+        #handles case of review text being super long and clickin the 'more...' button
+        await page.waitForSelector(".w8nwRe")
+        more_btn = await element.querySelector(".w8nwRe")
+        if more_btn is not None:
+            await page.evaluate("button => button.click()", more_btn)
+            await page.waitFor(5000)
+
         await page.waitForSelector('.MyEned')
         snippet = await element.querySelector('.MyEned')
         text = await page.evaluate('selected => selected.textContent', snippet)
